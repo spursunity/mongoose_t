c15d5f2b5d43705502f8252fd77ad7dd293372cf
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-08-13 18:28:14 +0300

    insert new instance

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-(function m() {
+(async function m() {
   try {
     const mongooseConnect = require('./config');
-    mongooseConnect();
+    const Car = require('./models/car');
+
+    await mongooseConnect();
+    const response = await Car.create({ brand: 'Audi', year: 2011 });
+    console.log(response);
   } catch (e) {
     console.log(e);
   }
