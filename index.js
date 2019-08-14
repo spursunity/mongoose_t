16500f2d9d4309db853338e160e0b1c88ffee466
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-08-14 09:42:01 +0300

    find instances

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-(function m() {
+(async function m() {
   try {
+    const Car = require('./models/car');
     const mongooseConnect = require('./config');
-    mongooseConnect();
+    await mongooseConnect();
+
+    // const response = await Car.find({});
+    const response = await Car.findOne({});
+    console.log(response);
   } catch (e) {
     console.log(e);
   }
